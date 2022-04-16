8cf81e6761d1a98314a5377cfe4384665936971f
ayrton <ayrton.parkinson1@gmail.com>
2022-04-16 12:37:04 -0400

    Refactor #index queries for dropdown menus

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,13 +1,14 @@
 class FlightsController < ApplicationController
   def index
-    @departing_airports = Airport.joins(:departing_flights).distinct
-    @departing_airports = @departing_airports.map { |da| [da.airport_code, da.id] }
+    @flights = Flight.joins('INNER JOIN airports departure_airports
+                             ON departure_airports.id = flights.departure_id
+                             INNER JOIN airports destination_airports
+                             ON destination_airports.id = flights.destination_id')
 
-    @arrival_airports = Airport.joins(:arriving_flights).distinct
-    @arrival_airports = @arrival_airports.map { |aa| [aa.airport_code, aa.id] }
-
-    @flight_dates = Flight.all.order(:start_time)
-    @flight_dates = @flight_dates.map { |f| [format_flight_date(f.start_time)] }.uniq
+    @departures = @flights.distinct.pluck('departure_airports.airport_code', 'flights.departure_id')
+    @destinations = @flights.distinct.pluck('destination_airports.airport_code', 'flights.destination_id')
+    @flight_dates = @flights.order(:start_time).pluck('flights.start_time')
+    @flight_dates = @flight_dates.map { |date| [format_flight_date(date)] }.uniq
   end
 
   private
