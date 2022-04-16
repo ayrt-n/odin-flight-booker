b2908f9c97856c449b6e349d77501dfe22b861c6
ayrton <ayrton.parkinson1@gmail.com>
2022-04-16 13:11:51 -0400

    Add flight query is query strings present

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -9,6 +9,10 @@ class FlightsController < ApplicationController
     @destinations = @flights.distinct.pluck('destination_airports.airport_code', 'flights.destination_id')
     @flight_dates = @flights.order(:start_time).pluck('flights.start_time')
     @flight_dates = @flight_dates.map { |date| [format_flight_date(date)] }.uniq
+
+    unless request.query_string.empty?
+      @flight_results = @flights.flight_from(params[:departing_id]).flight_to(params[:destination_id]).on_date(params[:start_time])
+    end
   end
 
   private
