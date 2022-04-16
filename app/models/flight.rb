1704a7fb0b53902857d7a44aed0e39fb1280fce5
ayrton <ayrton.parkinson1@gmail.com>
2022-04-16 13:11:09 -0400

    Add scopes to query based on from, to, and date

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
+  scope :flight_from, ->(id) { where('departure_id = ?', id) }
+  scope :flight_to, ->(id) { where('destination_id = ?', id) }
+  scope :on_date, ->(date) { where(start_time: date.to_date.beginning_of_day..date.to_date.end_of_day) }
+
   belongs_to :departure, class_name: 'Airport'
   belongs_to :destination, class_name: 'Airport'
 end
