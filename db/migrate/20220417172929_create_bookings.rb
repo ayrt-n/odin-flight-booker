59e702751a3abd10705e9c8e8e591ee52dc4fdb7
ayrton <ayrton.parkinson1@gmail.com>
2022-04-17 14:32:12 -0400

    Reverse belongs_to association between Booking and Passenger

--- db/migrate/20220417172929_create_bookings.rb
+++ db/migrate/20220417172929_create_bookings.rb
@@ -2,7 +2,6 @@ class CreateBookings < ActiveRecord::Migration[7.0]
   def change
     create_table :bookings do |t|
       t.references :flight
-      t.references :passenger
       
       t.timestamps
     end
