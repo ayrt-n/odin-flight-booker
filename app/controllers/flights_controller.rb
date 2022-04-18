d9bc048665148020c18fb39a113c444c7c97de35
ayrton <ayrton.parkinson1@gmail.com>
2022-04-18 08:41:31 -0400

    Simplify implementation of drop down search menus

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,14 +1,10 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.joins('INNER JOIN airports departure_airports
-                             ON departure_airports.id = flights.departure_id
-                             INNER JOIN airports destination_airports
-                             ON destination_airports.id = flights.destination_id')
-
-    @departures = @flights.distinct.pluck('departure_airports.airport_code', 'flights.departure_id')
-    @destinations = @flights.distinct.pluck('destination_airports.airport_code', 'flights.destination_id')
-    @flight_dates = @flights.order(:start_time).pluck('flights.start_time')
+    @flights = Flight.all.order(:start_time)
+    @flight_dates = @flights.pluck('flights.start_time')
     @flight_dates = @flight_dates.map { |date| [format_flight_date(date)] }.uniq
+
+    @airports = Airport.all.map { |airport| [airport.airport_code, airport.id] }
 
     unless request.query_string.empty?
       @flight_results = @flights.flight_from(params[:departing_id]).flight_to(params[:destination_id]).on_date(params[:start_time])
